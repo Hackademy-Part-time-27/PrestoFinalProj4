8455222f162cdceefedf9bfba8b8e497f1241fa2
Agostino Di Bartolomeo <agostinodibartolomeo@gmail.com>
2024-05-14 21:09:08 +0200

    Aggiunta componenti e button creazione annunci  nella home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,14 @@
     <div class="row">
         <div class="col-md-6 mx-auto">
             <div class="mt-4">
-                <h1>Benvenuto {{ auth()->user()->name }}</h1>
+                
+                <h1>Benvenuto  {{ auth()->user()->name }}</h1>
             </div>
+            @auth
+            <div class="mt-4">
+                <button class="btn btn-primary">Crea Annuncio</button>
+            </div>
+            @endauth
         </div>
     </div>
    
