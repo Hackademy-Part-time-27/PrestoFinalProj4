ffbc051dea34e33841c072a927c60cfa5608de5b
Marco Motolese <marcomotolese777@gmail.com>
2024-05-15 21:34:31 +0200

    aggiunta relazioni database

---
+++ app/Models/Announcement.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    protected $fillable=['title','body','price','user_id','category_id'];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    use HasFactory;
+}
