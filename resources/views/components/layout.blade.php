3136457bb384b302e33dcfa635db5bbf88a0f994
Riccardo Cardia <cardiariccardo94@gmail.com>
2024-05-19 23:36:59 +0200

    Piccole modifiche css contatti e navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,6 +16,7 @@
 
     <div>
         {{ $slot }}
+        
     </div>
     
     <x-footer/>
--- resources/views/contacts/contacts.blade.php
+++ resources/views/contacts/contacts.blade.php
@@ -1,30 +1,31 @@
 <x-layout> 
-<div class="row">
-        <div class="col-lg-6 mx-auto">
-            <h1>Presto.it support</h1>
-            <p class="lead text-muted">Contattaci per qualsiasi info!</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto pt-4">
+                <h1>Presto.it support</h1>
+                <p class="lead text-muted">Contattaci per qualsiasi info!</p>
 
-            <x-success />
+                <x-success />
 
-            <x-error />
-            
-            <form action="{{ route('contacts.send') }}" method="POST">
-                @csrf
-                <div class="row g-3">
-                    <div class="col-12">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control">
+                <x-error />
+                
+                <form action="{{ route('contacts.send') }}" method="POST">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label class="label-control fs-5 mb-2" for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control">
+                        </div>
+                        <div class="col-12">
+                            <label class="label-control fs-5 mb-2" for="message">Messaggio</label>
+                            <textarea name="message" id="message" rows="5" class="form-control"></textarea>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn-custom btn-primary">Invia</button>
+                        </div>
                     </div>
-                    <div class="col-12">
-                        <label for="message">Messaggio</label>
-                        <textarea name="message" id="message" rows="5" class="form-control"></textarea>
-                    </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn-custom btn-primary">Invia</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-   
 </x-layout> 
