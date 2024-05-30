6cb700c6c296f1f5af5b9c0c0d0e41d14c29b5e6
Riccardo Cardia <cardiariccardo94@gmail.com>
2024-05-30 20:08:42 +0200

    modifiche js e css

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,30 @@
 import 'bootstrap';
 
 
+
+
+window.addEventListener('scroll', () =>{
+    
+    let scroll = window.scrollY;
+    var nav = document.getElementById('nav').classList;
+
+    if (scroll >= 80){
+        nav.add('scrolled');
+    }else  {
+        nav.remove('scrolled');
+
+    }
+
+})
+
+window.addEventListener('load', ()=> {
+    let href = window.location.href
+    let cards = document.querySelectorAll('#card')
+    console.log(cards)
+    if(href.includes('http://127.0.0.1:8000/')){
+       for(let card of cards){
+        card.classList.add('swipe')
+       }
+    }
+ 
+})
