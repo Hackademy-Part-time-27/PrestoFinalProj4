2dd628b23929bd895ceb0ee1b26868d270eb03ca
Riccardo Cardia <cardiariccardo94@gmail.com>
2024-05-30 21:34:56 +0200

    modifiche crop

--- resources/views/announcement/list.blade.php
+++ resources/views/announcement/list.blade.php
@@ -14,7 +14,7 @@
                             <div class="col-4 mb-4 ">
                                 <div class="card-custom mx-4 my-3 card-swipe" id="card">
                                         <div class="div-img">
-                                            <img class="img-fluid rounded-1 w-100 ps-0" src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : 'https://picsum.photos/200' }}" class="card-img-top p-3 rounded" alt="...">
+                                            <img class="img-fluid rounded-1 w-100 ps-0" src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(300,300) : 'https://picsum.photos/200' }}" class="card-img-top p-3 rounded" alt="...">
                                         </div>
                                         <div class="text-start pt-2 div-text-card " >
                                             <h2 class="card-title text-3 border-bttm">{{$announcement->title}}</h2>
