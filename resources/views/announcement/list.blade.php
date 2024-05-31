e21df4313a7a16a188f491b18a2790e38a8fddc5
LeonardoRauco <leo.rauco65@outlook.it>
2024-05-31 17:47:53 +0200

    commit

--- resources/views/announcement/list.blade.php
+++ resources/views/announcement/list.blade.php
@@ -14,13 +14,13 @@
                             <div class="col-4 mb-4 ">
                                 <div class="card-custom mx-4 my-3">
                                         <div class="div-img">
-                                            <img class="img-fluid rounded-1 w-100 ps-0" src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : 'https://picsum.photos/200') }}" class="card-img-top p-3 rounded" alt="...">
+                                            <img class="img-fluid rounded-1 w-100 ps-0" src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : 'https://picsum.photos/200' }}" class="card-img-top p-3 rounded" alt="...">
                                         </div>
                                         <div class="text-start pt-2 div-text-card " >
                                             <h2 class="card-title text-3 border-bttm">{{$announcement->title}}</h2>
                                             <p class="card-text fs-4 text-danger ">{{ $announcement->category->name}} </p>
                                             <p class="card-text fs-5 border-bttm">{{$announcement->body}}</p>
-                                            <span class="text-end "><p class="card-text fs-3 text-danger pe-4 ">{{Number::currency($announcement->price, in: 'EUR', locale: 'de')}}</p></span>
+                                            <span class="text-end "><p class="card-text fs-2 text-danger pe-2 ">{{Number::currency($announcement->price, in: 'EUR', locale: 'de')}}</p></span>
                                             
                                             <div class="my-4">
                                                 <a href="{{ route('announcements.show', ['announcement' => $announcement->id]) }}" class="btn-custom text-decoration-none fs-4">Visualizza</a>
